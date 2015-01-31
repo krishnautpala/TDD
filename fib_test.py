5fa2b829ac44afce839fb40874411f225e59cfdf
krishnautpala <krishna9028@gmail.com>
2015-01-31 13:57:33 -0500

    added test cases

--- fib_test.py
+++ fib_test.py
@@ -3,5 +3,12 @@ import sys
 import fib
 
 # Get input from user
-number = input("Here you go Input Please  : ")
-print fib.Fib_recursion(number)
+#number = input("Here you go Input Please  : ")
+print fib.Fib_recursion(1)
+print fib.Fib_recursion(2)
+print fib.Fib_recursion(3)
+print fib.Fib_recursion(4)
+print fib.Fib_recursion(12)
+print fib.Fib_recursion(20)
+print fib.Fib_recursion(29)
+print fib.Fib_recursion(-1)
