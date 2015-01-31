80a5b684549baf817081fe07ea5a522ad9d3c6f6
krishnautpala <krishna9028@gmail.com>
2015-01-31 14:00:58 -0500

    negative test case resolved

--- fib.py
+++ fib.py
@@ -4,8 +4,10 @@ import sys
 # fib recurssion
 
 def Fib_recursion(number):
-    if (number == 1):
+    if (number == 1):  
 	return 0
     if (number == 2):   
 	return 1
+    if (number < 0):
+	return "Error"
     return (Fib_recursion(number-1)+Fib_recursion(number-2));
