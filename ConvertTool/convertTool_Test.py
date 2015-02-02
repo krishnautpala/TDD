8700db150a9b1ec4b7b057cc1c7f1dfc5dd1c736
krishnautpala <krishna9028@gmail.com>
2015-02-02 18:47:30 -0500

    added main program files and functional

---
+++ ConvertTool/convertTool_Test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import converTool
+
+print converTool.lb_to_kg(pounds)
+
+print converTool.kg_to_lb(kg)
+
+print converTool.lt_to_gal(litre)
+
+print converTool.gal_to_lt(gal)
+
+print converTool.km_to_mi(k_meter)
+
+print converTool.mi_to_km(mile)
+
