7bff8cf6fffc395d5fc21b6e9f36eb66fe85ed9e
krishnautpala <krishna9028@gmail.com>
2015-02-01 19:14:37 -0500

    updated winner function

--- TTT/TTT.py
+++ TTT/TTT.py
@@ -2,7 +2,8 @@
 import sys
 
 # Initialize a 3x3 table with -1
-table= [ [ 0 for i in range(3) ] for j in range(3) ]
+global table
+table = [ [ 0 for i in range(3) ] for j in range(3) ]
 
 # Displays current board
 def displayBoard():
@@ -11,11 +12,64 @@ def displayBoard():
 
 # Checks if the match is drawn
 def isDraw():    
+    # Check if board is full
+    for row in range(3):
+	for column in range(3):
+	    if (table[row][column] == 0):
+	    	return 1
+    # Check if Player1 has matrix complete
+    isPlayer1_won = isWinner(Player)
+    # Check if Player2 has matrix complete
+    isPlayer2_won = isWinner(Player)
+    # If both have complete return match drawn
+    if  isPlayer1_won == '0' and isPlayer2_won == '0':
+	return 0
 
-    return 0
+    return 1
 
 # Checks if a player is won
 def isWinner(Player):
+    global table
+    # Check if row is complete
+    # [x][x][x]
+    # [0][0][x]
+    # [x][x][0]
+    # Is row 1 complete
+    if (table[0][0] == ""  and table[0][1] == "" and table[0][2] ==""):
+	    return 1
+	# Is row 2 complete
+    if (table[1][0] == ""  and table[1][1] == "" and table[1][2] == ""):
+	    return 1
+	# Is row 3 complete
+    if (table[2][0] == ""  and table[2][1] == "" and table[2][2] == ""):
+	    return 1
+
+    # Check if column is complete
+    # [0][x][0]
+    # [0][x][x]
+    # [x][x][0]
+
+    # Is column 1 complete
+    if (table[0][0] == ""  and table[1][0] == "" and table[2][0] == ""):
+	    return 1
+    # Is column 2 complete
+    if (table[0][1] == ""  and table[1][1] == "" and table[2][1] == ""):
+	    return 1
+    # Is column 3 complete
+    if (table[0][2] == ""  and table[1][2] == "" and table[2][2] == ""):
+	    return 1
+
+    # Check if diagonal is complete
+    # [0][0][x]
+    # [0][x][x]
+    # [x][x][0]
+    # Is diagonal complete
+    if (table[0][0] == ""  and table[1][1] == "" and table[2][2] == ""):
+	    return 1
+	# Is diagonal complete
+    if (table[0][2] == ""  and table[1][1] == "" and table[2][0] == ""):
+	    return 1
+
     return 0
 
 # Inserts  a piece
@@ -24,6 +78,7 @@ def insertPiece(Piece):
 
 # Clear the board
 def clearBoard():
+    global table
     table = []
     return table
 
@@ -31,7 +86,4 @@ def clearBoard():
 def createUser():
     Player_Name = raw_input ("Please Enter you name: ")
     return Player_Name
-
-
-
 
--- TTT/TTT_test.py
+++ TTT/TTT_test.py
@@ -8,9 +8,18 @@ Player_1 = TTT.createUser()
 #User 2
 Player_2 = TTT.createUser()
 
+# Displays the board
 print TTT.displayBoard()
+
+# Checks if the match is drawn
 print TTT.isDraw()
+
+# Checks if Player won the game
 print TTT.isWinner(Player_1)
+
+# Inserts Piece
 print TTT.insertPiece("O")
+
+# Clears Board
 print TTT.clearBoard()
 
